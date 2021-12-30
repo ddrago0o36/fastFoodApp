aa26b26197939051ec83629baaeb650430b58236
Dincher <Dinchitoo@gmail.com>
2021-12-30 15:02:43 +0200

    asd

--- fastFoodApp/app/src/main/java/com/example/fastfoodapp/BurgerActivity.java
+++ fastFoodApp/app/src/main/java/com/example/fastfoodapp/BurgerActivity.java
@@ -1,10 +1,11 @@
 package com.example.fastfoodapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class BurgerActivity extends AppCompatActivity {
 
@@ -12,5 +13,16 @@ public class BurgerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_burger);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
--- fastFoodApp/app/src/main/java/com/example/fastfoodapp/PizzaActivity.java
+++ fastFoodApp/app/src/main/java/com/example/fastfoodapp/PizzaActivity.java
@@ -1,8 +1,13 @@
 package com.example.fastfoodapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class PizzaActivity extends AppCompatActivity {
 
@@ -11,4 +16,17 @@ public class PizzaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pizza);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
 }
